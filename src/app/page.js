97be07df327f42eebb54068651a9fd5ec5357a19
adamkow <adamkow8@hotmail.com>
2025-07-21 23:19:48 +0100

    font

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Fretboard from '@/components/Fretboard/Fretboard';
 export default function Home() {
   return (
     <main>
-      <h1 style={{ textAlign: 'center', color: '#fff', margin: '2rem' }}>24-Fret Guitar Fretboard</h1>
+      <h1 style={{ textAlign: 'center', color: '#fff', margin: '2rem' ,fontFamily: 'Arial, Helvetica, sans-serif', fontWeight: 'bold'}}>24-Fret Guitar Fretboard</h1>
       <Fretboard />
     </main>
   );

f0bdb1c4a25590ddb960a3b8c4697024c28facf1
adamkow <adamkow8@hotmail.com>
2025-07-21 23:35:31 +0100

    github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/fretboard',
+  assetPrefix: '/fretboard/',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
